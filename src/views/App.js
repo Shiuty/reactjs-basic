b7df756c8d2b8ca24df06a3f0bf05fbb6d062f9a
Nguyễn Hằng <nguyenhang@192.168.2.37>
2025-04-02 01:57:10 +0700

    change say hello

--- src/views/App.js
+++ src/views/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello world, welcome to ReactJS !!
+          Hello world, welcome to ReactJS Basic !!
         </p>
         <a
           className="App-link"
